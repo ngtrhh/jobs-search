4fe60a9fb3b84c842762e492e22a90004289bac3
ngtrhh <91332224+ngtrhh@users.noreply.github.com>
2024-04-15 22:29:34 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   const fetchJobs = async (keyword) => {
     fetch(
-      `http://jobs-search-server.vercel.app/?query=${keyword.query}&location=${keyword.location}`
+      `https://jobs-search-server.vercel.app/?query=${keyword.query}&location=${keyword.location}`
     )
       .then((res) => res.json())
       .then((res) => {
